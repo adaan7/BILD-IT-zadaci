22e29a0e34c9cdae4cdd2fb2bf17551dcaa504d3
pc <pc@Adan>
2017-02-12 00:42:43 +0100

    Zadaci 08.02.2017.

--- src/zadaci_08_02_2017/Zadatak05.java
+++ src/zadaci_08_02_2017/Zadatak05.java
@@ -15,10 +15,26 @@ public class Zadatak05 {
 
 		// petlja radi dok ne unesemo 0
 		while (number != 0) {
-			// uzimamo unos od korisnika
-			System.out
-					.print("Unesite cijeli broj (unos završava kada unesete 0): ");
-			number = input.nextInt();
+			// boolean varijabla koja ce kontrolisati petlju
+			boolean wrongUserInput = true;
+
+			// petlja radi dok je unos korisnika pogresan
+			while (wrongUserInput) {
+				try {
+					// uzimamo unos od korisnika
+					System.out
+							.print("Unesite cijeli broj (unos završava kada unesete 0): ");
+					number = input.nextInt();
+
+					// postavima boolean na false da bi izasli iz petlje
+					wrongUserInput = false;
+				} catch (Exception ex) {
+					// ako je uhvacen exception ispisujemo odgovarajucu poruku
+					System.out
+							.println("Pogresan unos. Morate unijeti cijeli broj.");
+					input.nextLine();
+				}
+			}
 
 			// ako je uneseni broj veci od trenutno najveceg broja, trenutni
 			// najveci broj postaje uneseni broj i brojac postavljamo na 1, a
@@ -31,10 +47,10 @@ public class Zadatak05 {
 				counter++;
 			}
 		}
-		
+
 		// zatvaramo scanner
 		input.close();
-		
+
 		// ispisujemo najveci broj i broj ponavljanja najveceg broja
 		System.out.println("Najveci broj je " + max + " i ponavlja se "
 				+ counter + " puta.");

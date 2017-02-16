37e4b4ed69f159bbfe5cf42f1a1818d4a2518c7f
pc <pc@Adan>
2017-02-16 13:59:08 +0100

    Zadaci 16.02.2017.

--- src/zadaci_16_02_2017/Zadatak02.java
+++ src/zadaci_16_02_2017/Zadatak02.java
@@ -22,30 +22,33 @@ public class Zadatak02 {
 		 * radianse. Širina i dužina u ovoj formuli se odnose na zapad i sjever.
 		 * Koristimo negativne vrijednosti da oznaèimo istok i jug.
 		 */
-		
+
 		Scanner input = new Scanner(System.in);
-		
+
 		// pozivamo metode koje uzimaju unos od korisnika
 		double x1 = DoubleUserInput.getDouble(input, "Unesite x1: ");
 		double y1 = DoubleUserInput.getDouble(input, "Unesite y1: ");
 		double x2 = DoubleUserInput.getDouble(input, "Unesite x2: ");
 		double y2 = DoubleUserInput.getDouble(input, "Unesite y2: ");
-		
+
 		input.close();
-		
+
 		final double RADIUS = 6371.01;
-		
+
 		// pretvaramo stepene u radianse
 		x1 = Math.toRadians(x1);
 		y1 = Math.toRadians(y1);
 		x2 = Math.toRadians(x2);
 		y2 = Math.toRadians(y2);
-		
+
 		// izracunamo udaljenost izmedju dvije unesene tacke
-		double distance = RADIUS * Math.acos(Math.sin(x1) * Math.sin(x2) + Math.cos(x1) * Math.cos(x2) * Math.cos(y1 - y2));
-		
+		double distance = RADIUS
+				* Math.acos(Math.sin(x1) * Math.sin(x2) + Math.cos(x1)
+						* Math.cos(x2) * Math.cos(y1 - y2));
+
 		// ispisujemo great circle distance
-		System.out.println("\nUdaljenost izmedju dvije tacke: " + distance + " km");
+		System.out.println("\nUdaljenost izmedju dvije tacke: " + distance
+				+ " km");
 
 	}
 
